f06a0086c22c44e8be373c955de57efeb16c4856
nilq <i.am.niels.nielsen@gmail.com>
2019-08-08 15:33:05 +0200

    Add code-review comments and formatting

--- src/hns.rs
+++ src/hns.rs
@@ -1,28 +1,40 @@
+// NOTER FRA NIELS:
+// - Jeg har udskiftet den implementerede type med `Self`, overhovedet ikke et krav men mere **sugar**.
+// - Brugte `cargo fmt` til at formattere koden.
+// - Nogen steder kunne man godt have undladt semikolon, ved f.eks. `+=` m.m., når det står sidst i en funktion.
+// ... Men det er nogenlunde smag og behag, da man alligevel skal tilføje semikolon, hvis man skriver videre på funktionen.
+// Har også implementeret default `Debug` på `Hector` og `Star` - så slipper man for manuelt at printe stats. Sorry, hvis det var intentionen at de skulle
+// ... printes anderledes.
 
-#[derive(Copy, Clone)]
-pub struct Hector { // Mathematical vector is called Hector in order not to confuse it with a Rust
-                // vector. We could probably use a crate, but this is more fun as an exercise.
+#[derive(Copy, Clone, Debug)]
+pub struct Hector {
+    // Mathematical vector is called Hector in order not to confuse it with a Rust
+    // vector. We could probably use a crate, but this is more fun as an exercise.
     pub x: f32,
     pub y: f32,
-    pub z: f32
+    pub z: f32,
 }
 
 impl Hector {
-    pub fn new() -> Hector {
-        Hector {x:0.0, y:0.0, z:0.0}
+    pub fn new() -> Self {
+        Self {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
     }
     // Adds another Hector to current Hector
-    pub fn add_change(&mut self, other_hector: &Hector) {
+    pub fn add_change(&mut self, other_hector: &Self) {
         self.x += other_hector.x;
         self.y += other_hector.y;
         self.z += other_hector.z;
     }
 
-    pub fn add(&self, other_hector: &Hector) -> Hector {
-        Hector {
+    pub fn add(&self, other_hector: &Self) -> Self {
+        Self {
             x: self.x + other_hector.x,
             y: self.y + other_hector.y,
-            z: self.z + other_hector.z
+            z: self.z + other_hector.z,
         }
     }
 
@@ -32,11 +44,11 @@ impl Hector {
         self.z *= number;
     }
 
-    pub fn multiply(&self, number: f32) -> Hector {
-        Hector {
+    pub fn multiply(&self, number: f32) -> Self {
+        Self {
             x: self.x * number,
             y: self.y * number,
-            z: self.z * number
+            z: self.z * number,
         }
     }
 
@@ -46,19 +58,19 @@ impl Hector {
         self.z /= number;
     }
 
-    pub fn divide_by(&self, number: f32) -> Hector {
-        Hector {
+    pub fn divide_by(&self, number: f32) -> Self {
+        Self {
             x: self.x / number,
             y: self.y / number,
-            z: self.z / number
+            z: self.z / number,
         }
     }
 
-    pub fn cross(&self, other_hector: &Hector) -> Hector {
+    pub fn cross(&self, other_hector: &Self) -> Self {
         Hector {
             x: (self.y * other_hector.z) - (self.z * other_hector.y),
             y: (self.z * other_hector.x) - (self.x * other_hector.z),
-            z: (self.x * other_hector.y) - (self.y * other_hector.x)
+            z: (self.x * other_hector.y) - (self.y * other_hector.x),
         }
     }
 
@@ -67,15 +79,14 @@ impl Hector {
     }
 }
 
-impl PartialEq for Hector { //Makes it so you can see if A and B's xyz are the same with A == B
+impl PartialEq for Hector {
+    //Makes it so you can see if A and B's xyz are the same with A == B
     fn eq(&self, other: &Hector) -> bool {
-        self.x == other.x &&
-        self.y == other.y &&
-        self.z == other.z
+        self.x == other.x && self.y == other.y && self.z == other.z
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct Star {
     pub mass: f32,
     pub pos: Hector,
@@ -85,36 +96,36 @@ pub struct Star {
 }
 
 impl Star {
-    pub fn new() -> Star {
-        Star {
+    pub fn new() -> Self {
+        Self {
             mass: 1.0,
             pos: Hector::new(),
             vel: Hector::new(),
             acc: Hector::new(),
-            color: [1.0, 1.0, 1.0]
+            color: [1.0, 1.0, 1.0],
         }
     }
 
-    pub fn find_vel(&mut self, timestep: f32) { // Simple Euler integration: v = v + a*dt
+    pub fn find_vel(&mut self, timestep: f32) {
+        // Simple Euler integration: v = v + a*dt
         self.vel.add_change(&self.acc.multiply(timestep));
     }
 
-    pub fn find_pos(&mut self, timestep: f32) { // Simple Euler integration: s = s + v*dt
+    pub fn find_pos(&mut self, timestep: f32) {
+        // Simple Euler integration: s = s + v*dt
         self.pos.add_change(&self.vel.multiply(timestep));
     }
 
-    pub fn acc_towards(&mut self, other_star: &Star) {
+    pub fn acc_towards(&mut self, other_star: &Self) {
         if self.pos != other_star.pos {
             let distance = self.pos.multiply(-1.0).add(&other_star.pos);
-            let distance = distance.multiply(other_star.mass/distance.length().powi(3));
+            let distance = distance.multiply(other_star.mass / distance.length().powi(3));
             self.acc.add_change(&distance);
         }
     }
 
     pub fn print_stats(&self) {
-        println!("M: {:?} P: {:?} {:?} {:?} V: {:?} {:?} {:?} A: {:?} {:?} {:?}",
-        self.mass, self.pos.x, self.pos.y, self.pos.z, self.vel.x, self.vel.y, self.vel.z,
-        self.acc.x, self.acc.y, self.acc.z);
+        println!("{:#?}", self) // NIELS: Tjek `Debug`
     }
 }
 
@@ -122,25 +133,33 @@ impl Star {
 pub struct Sector {
     pub total_mass: f32,
     pub center: Hector,
-    pub star_list: Vec<Star>
+    pub star_list: Vec<Star>,
 }
 
 impl Sector {
-    pub fn new() -> Sector { //Makes a new, empty Sector.
-        Sector {
+    pub fn new() -> Self {
+        //Makes a new, empty Sector.
+        Self {
             total_mass: 0.0,
             center: Hector::new(),
-            star_list: Vec::new()
+            star_list: Vec::new(),
         }
     }
 
-    pub fn add_star(&mut self, new_star: Star) { //Adds a single star to a Sector.
+    pub fn add_star(&mut self, new_star: Star) {
+        //Adds a single star to a Sector.
         self.total_mass += new_star.mass;
-        self.center.add_change(&new_star.pos.multiply(new_star.mass).divide_by(self.total_mass));
+        self.center.add_change(
+            &new_star
+                .pos
+                .multiply(new_star.mass)
+                .divide_by(self.total_mass),
+        );
         self.star_list.push(new_star);
     }
 
-    pub fn add_multiple_stars(&mut self, new_stars: Vec<Star>) { //Adds multiple stars to a Sector
+    pub fn add_multiple_stars(&mut self, new_stars: Vec<Star>) {
+        //Adds multiple stars to a Sector
         let mut center_mod = Hector::new();
         for star in new_stars {
             self.total_mass += star.mass;
@@ -151,23 +170,26 @@ impl Sector {
         self.center.add_change(&center_mod);
     }
 
-    pub fn as_star(&self) -> Star { //Makes a Star with the Sector's properties.
+    pub fn as_star(&self) -> Star {
+        //Makes a Star with the Sector's properties.
         Star {
             mass: self.total_mass,
             pos: self.center,
             vel: Hector::new(),
             acc: Hector::new(),
-            color: [1.0, 1.0, 1.0]
+            color: [1.0, 1.0, 1.0],
         }
     }
 
-    pub fn acc_reset(&mut self) { //Sets the acceleration of all Stars in the Sector to 0.
+    pub fn acc_reset(&mut self) {
+        //Sets the acceleration of all Stars in the Sector to 0.
         for n in 0..self.star_list.len() {
             self.star_list[n].acc = Hector::new();
         }
     }
 
-    pub fn internal_acc(&mut self) { //Accelerates all Stars in the Sector towards each other.
+    pub fn internal_acc(&mut self) {
+        //Accelerates all Stars in the Sector towards each other.
         for n in 0..self.star_list.len() {
             let mut current_star = self.star_list[n];
             for m in 0..self.star_list.len() {
@@ -177,9 +199,10 @@ impl Sector {
         }
     }
 
-    pub fn external_acc(&mut self, target: &Star) { //Accelerates all Stars in the Sector
-                                                    //towards the target. use as_star if you
-                                                    //want to target another Sector.
+    pub fn external_acc(&mut self, target: &Star) {
+        //Accelerates all Stars in the Sector
+        //towards the target. use as_star if you
+        //want to target another Sector.
         if self.center != target.pos {
             for n in 0..self.star_list.len() {
                 self.star_list[n].acc_towards(target);
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
-extern crate rand;
+// NOTER FRA NIELS:
+// I kan tjekke crate'en `colored`, til at få nice farver på infotekst.
+
 extern crate crossbeam;
-use rand::prelude::*;
+extern crate rand;
+
 use nannou::prelude::*;
+use rand::prelude::*;
 use std::io::Write;
 
 extern crate n_body;
 use n_body::hns;
 
+
+// Som alternativ til en masse macro-kald, kan vi her gemme den fulde fejlbesked som en `&'static str`
+// ... med denne lifetime, bliver teksten skrevet direkte, råt ind i den compilede binary. :)
+const ERROR_MESSAGE: &'static str = r"
+This program simulates a collision of two star clusters (CL1 and CL2)
+As input it needs the number of stars in each cluster, their radii
+ and the initial position and velocity of CL2.
+CL1 starts at rest at origo.
+Ex: n_body #_of_stars_CL1 #_of_stars_CL2 radiusCL1 radiusCL2 x y z vx vy vz
+Ex: n_body 1500 100 3000.0 2000.0 6000.0 0.0 0.0 -1.0 0.0 0.0
+";
+
 fn main() {
-    nannou::app(model)
-        .update(update)
-        .view(view)
-        .run();
+    nannou::app(model).update(update).view(view).run();
     // nannou::sketch(view);
 }
 
@@ -24,19 +37,14 @@ struct Model {
     divider: f32,
 }
 
-
 fn model(app: &App) -> Model {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 11 {
-        writeln!(std::io::stderr(), "This program simulates a collision of two star clusters (CL1 and CL2)").unwrap();
-        writeln!(std::io::stderr(), "As input it needs the number of stars in each cluster, their radii").unwrap();
-        writeln!(std::io::stderr(), " and the initial position and velocity of CL2.").unwrap();
-        writeln!(std::io::stderr(), "CL1 starts at rest at origo.").unwrap();
-        writeln!(std::io::stderr(), "Ex: n_body #_of_stars_CL1 #_of_stars_CL2 radiusCL1 radiusCL2 x y z vx vy vz").unwrap();
-        writeln!(std::io::stderr(), "Ex: n_body 1500 100 3000.0 2000.0 6000.0 0.0 0.0 -1.0 0.0 0.0").unwrap();
-        std::process::exit(1);
+        writeln!(std::io::stderr(), "{}", ERROR_MESSAGE).unwrap();
+        std::process::exit(1)
     }
+
     println!("{:?}", args);
 
     // app.main_window().set_inner_size_points(720.0, 720.0);
@@ -45,16 +53,25 @@ fn model(app: &App) -> Model {
     // draw.background().color(BLACK);
 
     let scale = 10.0; //Increases the distances and the speed of the simulation
-    // let number_of_stars: usize = 500; // Number of stars
+                      // let number_of_stars: usize = 500; // Number of stars
     let timestep = 1.0 * scale; // Time in Mega year
     let divider = 5.0 * scale; //Zoom factor
-    // let radius_of_cluster =  30.0 *scale; //Radius of cluster
+                               // let radius_of_cluster =  30.0 *scale; //Radius of cluster
     let number_of_stars = args[1].trim().parse::<usize>().unwrap() as usize;
     let radius_of_cluster = args[3].trim().parse::<f32>().unwrap();
 
-    let mut stars: Vec<hns::Star> = initialise_stars(number_of_stars, radius_of_cluster, [0.5, 0.5, 1.0]);
-    let cluster_center = hns::Hector{x:0.0, y:0.0, z:0.0};
-    let cluster_vel = hns::Hector{x:0.0, y:0.0, z:0.0};
+    let mut stars: Vec<hns::Star> =
+        initialise_stars(number_of_stars, radius_of_cluster, [0.5, 0.5, 1.0]);
+    let cluster_center = hns::Hector {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    let cluster_vel = hns::Hector {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
     set_center_and_vel(&mut stars, cluster_center, cluster_vel);
 
     let number_of_stars = args[2].trim().parse::<usize>().unwrap() as usize;
@@ -66,11 +83,15 @@ fn model(app: &App) -> Model {
     let vy = args[9].trim().parse::<f32>().unwrap();
     let vz = args[10].trim().parse::<f32>().unwrap();
 
-    let mut stars2: Vec<hns::Star> = initialise_stars(number_of_stars, radius_of_cluster, [1.0, 0.5, 0.5]);
-    let cluster_center = hns::Hector{x:x, y:y, z:z};
-    let cluster_vel = hns::Hector{x:vx, y:vy, z:vz};
+    let mut stars2: Vec<hns::Star> =
+        initialise_stars(number_of_stars, radius_of_cluster, [1.0, 0.5, 0.5]);
+    let cluster_center = hns::Hector { x: x, y: y, z: z };
+    let cluster_vel = hns::Hector {
+        x: vx,
+        y: vy,
+        z: vz,
+    };
     set_center_and_vel(&mut stars2, cluster_center, cluster_vel);
-
 
     let cam_xy = app
         .new_window()
@@ -118,7 +139,7 @@ fn update(_app: &App, m: &mut Model, _update: Update) {
     {
         let timestep = m.timestep;
         let groups: Vec<&mut [hns::Sector]> = m.sectors.chunks_mut(sectors_per_group).collect();
-        crossbeam::scope(|spawner|{
+        crossbeam::scope(|spawner| {
             for group in groups.into_iter() {
                 spawner.spawn(|_| {
                     for sec in group {
@@ -134,7 +155,8 @@ fn update(_app: &App, m: &mut Model, _update: Update) {
                     }
                 });
             }
-        }).unwrap();
+        })
+        .unwrap();
     }
 
     // for sec in &mut m.sectors {
@@ -159,42 +181,59 @@ fn update(_app: &App, m: &mut Model, _update: Update) {
 }
 
 fn view(app: &App, m: &Model, frame: &Frame) {
+    // NIELS: Her ville jeg måske bare bruge `if`, i stedet for at pakke dem ind i en match
     match frame.window_id() {
         id if id == m.cam_xy => {
             let draw_xy = app.draw_for_window(m.cam_xy).unwrap();
             draw_xy.background().color(BLACK); // Comment this out to activate tracks.
-            for sector in &m.sectors{
+            for sector in &m.sectors {
                 for star in &sector.star_list {
-                    draw_xy.ellipse().x_y(star.pos.x / m.divider, star.pos.y / m.divider).radius(star.mass).color(Rgb::new(star.color[0], star.color[1], star.color[2]));
+                    draw_xy
+                        .ellipse()
+                        .x_y(star.pos.x / m.divider, star.pos.y / m.divider)
+                        .radius(star.mass)
+                        .color(Rgb::new(star.color[0], star.color[1], star.color[2]));
                 }
             }
             draw_xy.to_frame(app, &frame).unwrap();
-        },
+        }
         id if id == m.cam_xz => {
             let draw_xz = app.draw_for_window(m.cam_xz).unwrap();
             draw_xz.background().color(BLACK); // Comment this out to activate tracks.
-            for sector in &m.sectors{
+            for sector in &m.sectors {
                 for star in &sector.star_list {
-                    draw_xz.ellipse().x_y(star.pos.x / m.divider, star.pos.z / m.divider).radius(star.mass).color(Rgb::new(star.color[0], star.color[1], star.color[2]));
+                    draw_xz
+                        .ellipse()
+                        .x_y(star.pos.x / m.divider, star.pos.z / m.divider)
+                        .radius(star.mass)
+                        .color(Rgb::new(star.color[0], star.color[1], star.color[2]));
                 }
             }
             draw_xz.to_frame(app, &frame).unwrap();
-        },
+        }
         id if id == m.cam_yz => {
             let draw_yz = app.draw_for_window(m.cam_yz).unwrap();
             draw_yz.background().color(BLACK); // Comment this out to activate tracks.
-            for sector in &m.sectors{
+            for sector in &m.sectors {
                 for star in &sector.star_list {
-                    draw_yz.ellipse().x_y(star.pos.y / m.divider, star.pos.z / m.divider).radius(star.mass).color(Rgb::new(star.color[0], star.color[1], star.color[2]));
+                    draw_yz
+                        .ellipse()
+                        .x_y(star.pos.y / m.divider, star.pos.z / m.divider)
+                        .radius(star.mass)
+                        .color(Rgb::new(star.color[0], star.color[1], star.color[2]));
                 }
             }
             draw_yz.to_frame(app, &frame).unwrap();
-        },
+        }
         _ => (),
     }
 }
 
-fn initialise_stars(number_of_stars: usize, radius_of_cluster: f32, color: [f32; 3]) -> Vec<hns::Star> {
+fn initialise_stars(
+    number_of_stars: usize,
+    radius_of_cluster: f32,
+    color: [f32; 3],
+) -> Vec<hns::Star> {
     // let radius_of_cluster: f32 = 3000.0;
     let mut stars: Vec<hns::Star> = vec![];
 
@@ -204,15 +243,15 @@ fn initialise_stars(number_of_stars: usize, radius_of_cluster: f32, color: [f32;
         newstar.color = color;
         let phi = thread_rng().gen_range(0.0, 6.28);
         let theta = thread_rng().gen_range(-3.14, 3.14);
-        newstar.pos=hns::Hector {
-            x: radius_of_cluster*theta.sin()*phi.cos(),
-            y: radius_of_cluster*theta.sin()*phi.sin(),
-            z: radius_of_cluster*theta.cos(),
+        newstar.pos = hns::Hector {
+            x: radius_of_cluster * theta.sin() * phi.cos(),
+            y: radius_of_cluster * theta.sin() * phi.sin(),
+            z: radius_of_cluster * theta.cos(),
         };
         newstar.vel = hns::Hector {
-            x: -newstar.pos.y/(0.21*radius_of_cluster),
-            y: newstar.pos.x/(0.32*radius_of_cluster),
-            z: 0.0
+            x: -newstar.pos.y / (0.21 * radius_of_cluster),
+            y: newstar.pos.x / (0.32 * radius_of_cluster),
+            z: 0.0,
         };
         stars.push(newstar)
     }
@@ -224,25 +263,33 @@ fn initialise_stars(number_of_stars: usize, radius_of_cluster: f32, color: [f32;
     stars
 }
 
-fn set_center_and_vel(stars: &mut Vec<hns::Star>, cluster_center: hns::Hector, cluster_vel: hns::Hector){
+fn set_center_and_vel(
+    stars: &mut Vec<hns::Star>,
+    cluster_center: hns::Hector,
+    cluster_vel: hns::Hector,
+) {
     for star in stars {
         star.pos.add_change(&cluster_center);
         star.vel.add_change(&cluster_vel);
-    };
+    }
 }
 
 fn make_sectors(mut star_list: Vec<hns::Star>, recursions_left: u32) -> Vec<hns::Sector> {
     if recursions_left > 0 {
         if 2u32.pow(recursions_left) as usize > star_list.len() {
-            panic!("The recursion depth {:?} is greater than the number of stars {:?}", recursions_left, star_list.len());
+            panic!(
+                "The recursion depth {:?} is greater than the number of stars {:?}",
+                recursions_left,
+                star_list.len()
+            );
         }
         star_list.sort_by(|a, b| match recursions_left % 3 {
             0 => a.pos.x.partial_cmp(&b.pos.x).unwrap(),
             1 => a.pos.y.partial_cmp(&b.pos.y).unwrap(),
             2 => a.pos.z.partial_cmp(&b.pos.z).unwrap(),
-            _ => panic!("recursions_left % 3 somehow resulted in something other than 0, 1, or 2")
+            _ => panic!("recursions_left % 3 somehow resulted in something other than 0, 1, or 2"),
         });
-        let (sub_list_1, sub_list_2) = star_list.split_at(star_list.len()/2);
+        let (sub_list_1, sub_list_2) = star_list.split_at(star_list.len() / 2);
         let mut sub_list_1 = make_sectors(sub_list_1.to_vec(), recursions_left - 1);
         let sub_list_2 = make_sectors(sub_list_2.to_vec(), recursions_left - 1);
         for sector in sub_list_2 {
@@ -252,6 +299,6 @@ fn make_sectors(mut star_list: Vec<hns::Star>, recursions_left: u32) -> Vec<hns:
     } else {
         let mut return_sector = hns::Sector::new();
         return_sector.add_multiple_stars(star_list);
-        vec!(return_sector)
+        vec![return_sector]
     }
 }
